2a7c9f42ad715ff973b5b34f6621e6364c78f566
Lyubo <lyubodzhamov@gmail.com>
2019-09-02 16:24:58 -0400

    Add AudioRecorder and AudioPlayer from google walkietalkie project

--- app/src/main/java/com/qeh/lyubo/qeh/Constants.java
+++ app/src/main/java/com/qeh/lyubo/qeh/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int USER_ADVERTISER = 0;
     public static final int USER_DISCOVERER = 1;
     public static String ServiceId = "com.lyubo.qandeh";
+    public static final String TAG = "QnEh";
 }
